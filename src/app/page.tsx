0ed7dc9b39cd5611c2d48c8115ee424d846290c4
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-05-16 12:11:27 -0300

    prettier and eslint setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/Button";
-
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <Button />
-    </>
+    <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
   )
 }
