183c591196d2028199eb9e61cf5f86c3e3c5e2a1
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-05-19 18:30:17 -0300

    criando nova memória

--- src/components/MediaPicker.tsx
+++ src/components/MediaPicker.tsx
@@ -20,6 +20,7 @@ export function MediaPicker() {
     <>
       <input
         onChange={onFileSelected}
+        name="coverUrl"
         type="file"
         id="media"
         className="invisible h-0 w-0"
